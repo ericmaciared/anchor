856147ec75b7c666654e4550b721ae8e2438f41c
Eric Macià <ericmaciared@gmail.com>
2025-07-12 21:10:39 +0200

    added background animation and improved icon

--- lib/features/habits/presentation/screens/habits_screen.dart
+++ lib/features/habits/presentation/screens/habits_screen.dart
@@ -1,6 +1,7 @@
 import 'package:anchor/features/habits/presentation/controllers/habit_controller.dart';
-import 'package:anchor/features/habits/presentation/widgets/habit_list_section.dart';
+import 'package:anchor/features/habits/presentation/widgets/habit_list.dart'; // Needed for HabitList
 import 'package:anchor/features/habits/presentation/widgets/habits_screen_app_bar.dart';
+import 'package:anchor/features/shared/quotes/presentation/widgets/daily_quote_card.dart'; // Needed for DailyQuoteCard
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,7 +18,7 @@ class _HabitsScreenState extends ConsumerState<HabitsScreen> {
   @override
   Widget build(BuildContext context) {
     final controller = HabitController(ref, context);
-    final todayHabits = controller.getAllSelectedHabits();
+    final selectedHabits = controller.getAllSelectedHabits();
 
     return Scaffold(
       appBar: HabitsScreenAppBar(
@@ -30,16 +31,29 @@ class _HabitsScreenState extends ConsumerState<HabitsScreen> {
             // Scrollable content
             Padding(
               padding: const EdgeInsets.only(top: 96),
-              child: todayHabits.isEmpty
+              child: selectedHabits.isEmpty
                   ? EmptyHabitState(onAdd: () {})
-                  : HabitListSection(
-                      habits: todayHabits,
-                      onToggleHabitCompletion: (habit) {
-                        controller.toggleHabitCompletion(habit);
-                      },
-                      onLongPress: (habit) {
-                        controller.showEditHabitModal(habit);
-                      },
+                  : ListView(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      children: [
+                        HabitList(
+                          habits: selectedHabits,
+                          label: 'Habits',
+                          onToggleHabitCompletion: (habit) {
+                            controller.toggleHabitCompletion(habit);
+                          },
+                          onLongPress: (habit) {
+                            controller.showEditHabitModal(habit);
+                          },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 16),
+                          child: Divider(),
+                        ),
+                        DailyQuoteCard(),
+                        const SizedBox(height: 112),
+                      ],
                     ),
             ),
           ],
--- lib/features/welcome/welcome_screen.dart
+++ lib/features/welcome/welcome_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rive/rive.dart' as rive;
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -7,38 +10,72 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (isDarkMode)
-                Image.asset(
-                  'assets/icon/app_logo_white.png',
-                  width: 100,
-                  height: 100,
-                ),
-              if (!isDarkMode)
-                Image.asset(
-                  'assets/icon/app_logo_black.png',
-                  width: 100,
-                  height: 100,
-                ),
-              Text(
-                'anchor.',
-                style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Stack(children: [
+              rive.RiveAnimation.asset(
+                'assets/animations/welcome.riv',
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 16),
-              TextButton(
-                onPressed: () {
-                  context.go('/tasks');
-                },
-                child: Text('Start'),
+              Positioned.fill(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 250, sigmaY: 250),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
               ),
-            ],
+            ]),
           ),
-        ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (isDarkMode)
+                    Image.asset(
+                      'assets/icon/app_logo_white.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                  if (!isDarkMode)
+                    Image.asset(
+                      'assets/icon/app_logo_black.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                  Text(
+                    'anchor.',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      shadows: [
+                        Shadow(
+                          blurRadius: 4.0,
+                          color: Colors.black.withAlpha(50),
+                          offset: const Offset(2.0, 2.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () {
+                      context.go('/tasks');
+                    },
+                    child: Text(
+                      'Start',
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
